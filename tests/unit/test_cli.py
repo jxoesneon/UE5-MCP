8c80a724bf5b2165c5a62f0818cfcd8888de8a02
Jose Eduardo Rojas Jimenez <joseeduardox@gmail.com>
2025-12-18 16:52:02 -0600

    chore(release): Bump version to v0.9.0

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -18,4 +18,4 @@ def test_cli_version_flag(capsys) -> None:
     captured = capsys.readouterr()
 
     assert rc == 0
-    assert captured.out.strip() == "0.8.0"
+    assert captured.out.strip() == "0.9.0"
